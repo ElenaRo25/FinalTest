7a049f27d4e42d6df65ad3032025fc25a2cf3b62
Elena <lunase@bk.ru>
2023-11-01 01:04:49 +0300

    Создание метода для разделения строки

---
+++ FinalTest/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача:
+// Написать программу,
+// которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше,
+// либо равна 3 символам. 
+
+// Решение:
+
+// Cоздаем метод для разделения строки из консоли в массив по знаку ","
+string[] GetArrayStringConsole (string inConsolSymbol)
+{
+    string[] arraySymbol = new string[inConsolSymbol.Length];
+    arraySymbol = inConsolSymbol.Split(",");
+    return arraySymbol;
+}
